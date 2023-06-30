339f4ccd4462b3a118e72fe8f91384c83d91c188
owensizemore <owensizemore9@gmail.com>
2023-06-30 10:34:55 -0400

    Adding simple translation from English to Spanish

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        prompt = request.form["prompt"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(prompt),
             temperature=0.6,
+            max_tokens=1024
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +23,5 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(prompt):
+    return "Translate the following text into Spanish: " + prompt
